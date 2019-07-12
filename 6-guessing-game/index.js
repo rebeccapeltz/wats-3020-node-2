a7a04ac7313e5769d770ff5e688929ab8d28fccb
rebeccapeltz <rebeccapeltz@gmail.com>
2019-07-12 15:32:01 -0700

    6-guessing

--- 6-guessing-game/index.js
+++ 6-guessing-game/index.js
@@ -4,17 +4,31 @@ Input:
 Output:
 Usage: 
 */
-//TODO create a variable 'n' that contains a random number between 1 and 10
+//create a variable 'n' that contains a random number between 1 and 10
+let n = Math.floor(Math.random() * 10) + 1 //get this randomly
 
-//TODO initialize guessCount to 0 and we'll allow only 3 guesses
+
+//initialize guessCount to 0 and we'll allow only 3 guesses
+let guessCount = 0
 
 const io = require("../modules/io/index")
 
 //TODO write a function 'getOrdinal' that takes an integer between 1 and 3 and returns "first","second","third" and return the input if not 1-3
-
+function getOrdinal(n){
+  if (n === 1){
+    return "first"
+  } else if (n===2){
+    return "second"
+  } else if (n===3) {
+    return "third"
+  }
+  else {
+    return n
+  }
+}
 
 //set the prompt message
-io.terminal.setPrompt('3 Guesses: Guess the number! (0-10): ');
+io.terminal.setPrompt('3 Guesses: Guess the number! (1-10): ');
 
 //run the prompt mesage in the terminal
 io.terminal.prompt();
@@ -31,8 +45,8 @@ io.terminal.on('line', function (response) {
   let responseInt = parseInt(response)
 
   // TODO write logical expressions to test for valid integer and that if guess equals random 'n'
-  if () {
-    if () {
+  if (responseInt !== NaN) {
+    if (responseInt === n) {
       console.log(`You win!! - ${responseInt} is the number`);
       process.exit(0);
     } else {
@@ -44,8 +58,8 @@ io.terminal.on('line', function (response) {
   // (optional) say something if number guessed is too small
 
   // if done guessing exit
-  //TODO write a logical expression to see if user has used up all guesses
-  if () {
+  //write a logical expression to see if user has used up all guesses
+  if (guessCount > 2) {
     console.log('You lose - game over');
     //report the correct value
     console.log(`correct value: ${n}`)
@@ -56,4 +70,7 @@ io.terminal.on('line', function (response) {
 })
 
 
-//TODO detect the terminal is closing and say something
+io.terminal.on('close', function () {
+  console.log(`DONE`)
+  process.exit();
+});
