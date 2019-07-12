9e3546289f6dc4ef27505e0cf3f273b5a88ef3cf
rebeccapeltz <rebeccapeltz@gmail.com>
2019-07-12 15:20:30 -0700

    4-char count

--- 4-character-count/index.js
+++ 4-character-count/index.js
@@ -6,29 +6,53 @@ Usage:
 */
 const io = require("../modules/io/index")
 
-//TODO init charcount object to count characters
+//init charcount object to count characters
+let charcount = {}
 
-//TODO set the prompt the enter a string
+//set the prompt the enter a string
+io.terminal.setPrompt("Enter a string to analyze: ")
+
 
 // call the prompt
 io.terminal.prompt()
 
 io.terminal.on("line", function (response) {
   //split the string to create an array
-  //TODO turn the response into an array of characters
+  let characters = response.split('')
 
   //TODO iterate through array to create object that has character for key and count for value
-  
+  for (let character of characters){
+    if (charcount[character]){
+      charcount[character] ++
+    } else {
+      charcount[character] = 1
+    }
+  }
+  /*
+  h: 1
+  e: 1
+  l: 2
+  o: 1
+  */
   io.terminal.close()
 })
 .on("close",function(){
   let charArr = []
-  //TODO convert object to array of objects by looping through characters in object
- 
   //TODO sort the array based on count
+  for (let character in charcount ){
+    charArr.push({char:character,count:charcount[character]})
+  }
+ 
+  charArr.sort(function(a,b){
+    if (a.count<b.count) return -1  //return -1 to indicate less than
+    else if (a.count>b.count) return 1  //return +1 to indicate greater than
+    else return 0  //return 0 to indicate equal
+  })
   
 
   io.print("Character count in ascending order:")
-  //TODO iterate through array and report the character and its count in ascending order 
-  
+  //iterate through array and report the character and its count in ascending order 
+  for (let row of charArr){
+    io.print (`${row.char} ${row.count}`)
+  }
 })
